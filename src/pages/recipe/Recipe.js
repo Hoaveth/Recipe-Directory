1b03f8a2fb3aef96c9d2d2b1e4c911e54fb4591a
Hoaveth <hoaveth.gaming@gmail.com>
2022-06-27 22:44:54 +0800

    [Update] - added the recipe list component and recipe detail view

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,8 +1,29 @@
+import React from "react";
+import { useFetch } from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
 //styles
 import "./Recipe.css";
 
-import React from "react";
-
 export default function Recipe() {
-  return <div>Recipe</div>;
+  const { id } = useParams();
+  const url = "http://localhost:3000/recipes/" + id;
+  const { data: recipe, isPending, error } = useFetch(url);
+  return (
+    <div className="recipe">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipe && (
+        <>
+          <h1>{recipe.title}</h1>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map((ingredient) => (
+              <li key={ingredient}>{ingredient}</li>
+            ))}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
+    </div>
+  );
 }
